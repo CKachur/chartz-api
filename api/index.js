86da76ebf3fe2fd088f34def60e85da41af323d0
ckachur <cameron@livio.io>
2018-07-31 14:39:28 -0400

    Allow access cross site.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ var bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 
 var endpoints = require('./endpoints')
 
